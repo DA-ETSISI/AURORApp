61a5a3770dda01d2b37c4048918dcc07c6b05b66
Alejandro Alonso Lopez <aalonsopersonal@gmail.com>
2024-09-03 22:54:11 +0200

    Deleted unused import

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,6 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 const cors = require('cors');
-const { get } = require('http');
 const app = express();
 
 app.use(express.json());
